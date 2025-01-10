ee87f8debf8fbbc3f9010bbe362506dad7539877
SudtipongKn <166700563+Teamkn2001@users.noreply.github.com>
2025-01-10 21:07:11 +0700

    Remove config.js from git tracking

--- src/api/guest.js
+++ src/api/guest.js
@@ -1,12 +1,13 @@
+import { API_ENDPOINTS } from '../../config';
 import axios from '../configs/axios';
 
-export const getPopularItems = () => axios.get('/getPopularItems')
-export const getRandomItems = () => axios.get('/getRandomItems')
-export const getPopularUsers = () => axios.get('/getPopularUsers')
+export const getPopularItems = () => axios.get(API_ENDPOINTS.POPULAR_ITEMS)
+export const getRandomItems = () => axios.get(API_ENDPOINTS.RANDOM_ITEMS)
+export const getPopularUsers = () => axios.get(API_ENDPOINTS.POPULAR_USERS)
 
 export const getUserById = async (userId) => {
-    const rs = await axios.get(`/getUser/${userId}`)
+    const rs = await axios.get(`${API_ENDPOINTS.GET_USER}/${userId}`)
     return rs.data
 }
-export const getUserByUsername = (username) => axios.get(`/searchUsers/${username}`)
-export const getItemById = (itemId) => axios.get(`/getItemById/${itemId}`)
+export const getUserByUsername = (username) => axios.get(`${API_ENDPOINTS.SEARCH_USERS}/${username}`)
+export const getItemById = (itemId) => axios.get(`${API_ENDPOINTS.GET_ITEM}/${itemId}`)
