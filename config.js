fd6dc5cf98e6d7c3a554d36408fbde93c1e492ec
SudtipongKn <166700563+Teamkn2001@users.noreply.github.com>
2025-01-10 21:13:24 +0700

    untrack API

---
+++ config.js
@@ -0,0 +1,10 @@
+
+export const API_ENDPOINTS = {
+    // guest api
+    POPULAR_ITEMS: '/getPopularItems',
+    RANDOM_ITEMS: '/getRandomItems',
+    POPULAR_USERS: '/getPopularUsers',
+    GET_USER: '/getUser',
+    SEARCH_USERS: '/searchUsers',
+    GET_ITEM: '/getItemById'
+  }
